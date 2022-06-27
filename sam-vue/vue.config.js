c15d0380e2ae282438511b34d298a8f34f65170e
sil-2017 <khachatryansilvard2@gmail.com>
2022-06-28 03:27:26 +0400

    first commit

---
+++ sam-vue/vue.config.js
@@ -0,0 +1,13 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+// module.exports = {
+//   css:{
+//     loaderOption:{
+//       sass:{
+//         prependData:'@import "@/assets/style.scss";'
+//       }
+//     }
+//   }
+// }
